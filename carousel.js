6de572d268e07a3af904a26c698977e0e269455b
jelly <chm073@gmail.com>
2020-03-18 22:02:04 +0900

    Implement next slide

--- carousel.js
+++ carousel.js
@@ -11,3 +11,19 @@ const carouselImageResize = () => {
 
 window.addEventListener('resize', carouselImageResize);
 window.addEventListener('DOMContentLoaded', carouselImageResize);
+
+document.getElementById('next-button').addEventListener('click', () => {
+  const previous = document.getElementsByClassName('previous')[0];
+  previous.classList.remove('previous');
+  previous.parentElement.appendChild(previous);
+
+  const current = document.getElementsByClassName('current')[0];
+  current.classList.remove('current');
+  current.classList.add('previous');
+
+  const next = document.getElementsByClassName('next')[0];
+  next.classList.remove('next');
+  next.classList.add('current');
+  setTimeout(() => next.nextElementSibling.classList.add('next'), 0);
+
+});
